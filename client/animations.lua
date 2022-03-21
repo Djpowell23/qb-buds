33b029429e243f038528e4c17386511bf3dc0e3e
Devin Powell <dpowel11@kent.edu>
2022-03-21 15:02:58 -0600

    adjusted joints, moved joints internally
    moved normal joint animation inside qb-buds script.

--- client/animations.lua
+++ client/animations.lua
@@ -1,15 +1,52 @@
 exports['qb-core']:GetCoreObject()
 
+local SmokingWeed = false 
+local RelieveCount = 0
+
 -- Emote Stuff -- 
+RegisterNetEvent('animations:client:SmokeWeed')
+AddEventHandler('animations:client:SmokeWeed', function()
+  SmokingWeed = true
+  Citizen.CreateThread(function()
+    while SmokingWeed do
+      Citizen.Wait(Config.JointReliefInterval * 1000)
+      TriggerServerEvent('hud:server:RelieveStress', math.random(Config.NormalJointMinStress, Config.NormalJointMaxStress))
+      RelieveCount = RelieveCount + 1
+      if RelieveCount == Config.JointMaxReliefCount then
+        if ChosenDict == "MaleScenario" and IsInAnimation then
+          ClearPedTasksImmediately(PlayerPedId())
+          IsInAnimation = false
+          DebugPrint("Forced scenario exit")
+        elseif ChosenDict == "Scenario" and IsInAnimation then
+          ClearPedTasksImmediately(PlayerPedId())
+          IsInAnimation = false
+          DebugPrint("Forced scenario exit")
+        end
+
+        if IsInAnimation then
+          ClearPedTasks(PlayerPedId())
+          DestroyAllProps()
+          IsInAnimation = false
+        end
+
+        if SmokingWeed then
+          SmokingWeed = false
+          RelieveCount = 0
+        end
+      end
+    end
+  end)
+end)
+
 RegisterNetEvent('animations:client:SmokeCaramelo')
 AddEventHandler('animations:client:SmokeCaramelo', function()
   SmokingWeed = true
   Citizen.CreateThread(function()
     while SmokingWeed do
-      Citizen.Wait(10000)
+      Citizen.Wait(Config.JointReliefInterval * 1000)
       TriggerServerEvent('hud:server:RelieveStress', math.random(Config.CarameloMinStressRelief, Config.CarameloMaxStressRelief))
       RelieveCount = RelieveCount + 1
-      if RelieveCount == 6 then
+      if RelieveCount == Config.JointMaxReliefCount then
         if ChosenDict == "MaleScenario" and IsInAnimation then
           ClearPedTasksImmediately(PlayerPedId())
           IsInAnimation = false
@@ -40,10 +77,11 @@ AddEventHandler('animations:client:SmokeMedical', function()
   SmokingWeed = true
   Citizen.CreateThread(function()
     while SmokingWeed do
-      Citizen.Wait(10000)
+      Citizen.Wait(Config.JointReliefInterval * 1000)
       TriggerServerEvent('hud:server:RelieveStress', math.random(Config.MedicalMinStressRelief, Config.MedicalMaxStressRelief))
+      SetEntityHealth(PlayerPedId(), GetEntityHealth(PlayerPedId()) + Config.MedicalHealingAmount)
       RelieveCount = RelieveCount + 1
-      if RelieveCount == 6 then
+      if RelieveCount == Config.JointMaxReliefCount then
         if ChosenDict == "MaleScenario" and IsInAnimation then
           ClearPedTasksImmediately(PlayerPedId())
           IsInAnimation = false
@@ -74,10 +112,10 @@ AddEventHandler('animations:client:SmokeHeaven', function()
   SmokingWeed = true
   Citizen.CreateThread(function()
     while SmokingWeed do
-      Citizen.Wait(10000)
+      Citizen.Wait(Config.JointReliefInterval * 1000)
       TriggerServerEvent('hud:server:RelieveStress', math.random(Config.HeavenMinStressRelief, Config.HeavenMaxStressRelief))
       RelieveCount = RelieveCount + 1
-      if RelieveCount == 6 then
+      if RelieveCount == Config.JointMaxReliefCount then
         if ChosenDict == "MaleScenario" and IsInAnimation then
           ClearPedTasksImmediately(PlayerPedId())
           IsInAnimation = false
